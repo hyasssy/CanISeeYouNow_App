cf9add2f0bc35c4038716bd7ba5c9975a5d06795
Yuto Hayashi <hyasssy@gmail.com>
2021-04-26 18:17:56 +0900

    APIテスト用シーン構築

---
+++ CanIseeYounow_PC_UnityProject/Assets/StreetViewTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Networking;
+using Cysharp.Threading.Tasks;
+using UniRx;
+using UnityEngine.UI;
+
+public class StreetViewTest : MonoBehaviour
+{
+    [SerializeField]
+    Image image;
+    private void Awake()
+    {
+        // var value = CryptExpansion.Encrypt();
+        // Debug.Log(value);
+        // var result =
+    }
+    async private UniTask Start()
+    {
+        var result = await CheckPassword.CollatePassWord(/*_password.text*/"pass", (progress) => Debug.Log("進捗(0-1)=" + progress));
+        var locationData = await StreetViewAPI.GetLocationData("35.70140757816516,139.4838876444111");
+        var tex = await StreetViewAPI.GetTexture(512, 512, locationData.PanoId, 0, 0, "outdoor");
+        image.sprite = Sprite.Create(tex, new Rect(0, 0, tex.width, tex.height), Vector2.zero);
+    }
+}
